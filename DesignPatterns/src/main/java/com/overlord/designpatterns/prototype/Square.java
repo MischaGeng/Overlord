dea37cf9bc1b43a7732828edf75b4bd9431f73da
yueming0118 <8810520zao>
2019-06-21 18:09:17 +0800

    原型模式

---
+++ DesignPatterns/src/main/java/com/overlord/designpatterns/prototype/Square.java
@@ -0,0 +1,12 @@
+package com.overlord.designpatterns.prototype;
+
+public class Square extends Shape {
+    public Square() {
+        type = "Square";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
